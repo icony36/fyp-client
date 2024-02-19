6e05d54b075df4ea67fbaf2b4c93eadd2b45fed6
IT <icode67658@gmail.com>
2024-02-19 11:37:05 +0800

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -353,18 +353,22 @@ export const getResponsesObj = (responses) => {
 };
 
 export const checkIsURL = (str) => {
-  var urlPattern = new RegExp(
-    "^(https?:\\/\\/)?" + // validate protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i"
-  );
+  // var urlPattern = new RegExp(
+  //   "^(https?:\\/\\/)?" + // validate protocol
+  //     "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+  //     "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
+  //     "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
+  //     "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
+  //     "(\\#[-a-z\\d_]*)?$",
+  //   "i"
+  // );
+
+  const urlPattern = /^(https?:\/\/)?([\w.-]+)\.([a-z]{2,})(\/\S*)?$/i;
+
+  const mapPattern = /(https|http):\/\/(www\.|)google\.[a-z]+\/maps/;
 
   // validate fragment locator
-  return !!urlPattern.test(str);
+  return urlPattern.test(str) || mapPattern.test(str);
 };
 
 export const delay = (ms) => new Promise((res) => setTimeout(res, ms));
